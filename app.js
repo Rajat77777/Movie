f6bc12f79c57badaaec2e9731dcf69a23d0493b5
Rajatsubhro <rajatsubhrochowdhury2000@gmail.com>
2024-08-02 22:12:41 +0530

    ki error eshe jache ke jane

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/user-routes";
+
+dotenv.config();
+const app = express();
+
+app.use("/user",userRouter);
+
+
+
+
+mongoose
+  .connect(
+    `mongodb+srv://rajatsubhrochowdhury2000:sagl2GLBcXiK8cfz@cluster1.wjem02e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`
+  )
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Connected to database and server is running")
+    )
+  )
+  .catch((e) => console.log(e));
+
