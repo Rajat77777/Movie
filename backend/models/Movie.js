9f982a5c065feeb07d9da7e25518279dda48c40d
Rajatsubhro <rajatsubhrochowdhury2000@gmail.com>
2024-08-27 23:57:13 +0530

    created movie object and further working on it. Still some work left

---
+++ backend/models/Movie.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+
+    },
+    description:{
+        type:String, 
+        required:true,
+    },
+    releaseDate:{
+        type:Date,
+        required:true,
+    },
+    posterUrl:{
+        type:String, 
+        required:true,
+    },
+    featured:{
+        type:Boolean
+    },
+    bookings:[{type:String}],
+    admin: {
+        type:String,
+        required:true,
+    },
+
+});
