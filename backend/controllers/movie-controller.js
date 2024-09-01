bdde1f63f11a4720c958e3d5cdad1a00a0ada9af
Rajatsubhro <rajatsubhrochowdhury2000@gmail.com>
2024-09-01 20:28:21 +0530

    decrypted id

--- backend/controllers/movie-controller.js
+++ backend/controllers/movie-controller.js
@@ -1,8 +1,22 @@
+import jwt from "jsonwebtoken";
+
 export const addMovie = async(req,res,next)=>{
     const extractedToken = req.headers.authorization.split(" ")[1];
     if(!extractedToken && extractedToken.trim()===""){
         return res.status(404).json({message:"Token not Found"});
     }
   let adminId;  
+
+
+  jwt.verify(extractedToken,process.env.SECRET_KEY,(err,decrypted)=>{
+    if(err){
+      return res.status(400).json({message:`${err.message}`})
+    }else{
+      adminId = decrypted.id;
+    }
+  })
+
+
+  //create a new movie
 
 };
